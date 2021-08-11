5e02870e9c3b79a61b4993c67e60aba670645e61
Alexander Pankratov <s.pankratoff@gmail.com>
2021-08-11 21:06:59 +0300

    Feat: puppeteer 10

--- src/Resources/CDPSession.php
+++ src/Resources/CDPSession.php
@@ -3,6 +3,8 @@
 namespace Nesk\Puphpeteer\Resources;
 
 /**
+ * @method mixed connection()
+ * @method-extended mixed connection()
  * @method mixed send(mixed $method, mixed ...$paramArgs)
  * @method-extended mixed send(mixed $method, mixed ...$paramArgs)
  * @method void detach()
